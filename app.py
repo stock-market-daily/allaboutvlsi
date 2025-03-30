8cf315be56bdb7a1315f22204211c1316d7fd5d3
stock-market-daily <stock.market.daily.2024@gmail.com>
2025-03-30 20:27:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,4 +86,7 @@ def chat():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    #app.run(debug=True, port=5000)
+
+    port = int(os.environ.get("PORT", 5000))  # Render assigns a dynamic port
+    app.run(host="0.0.0.0", port=port, debug=True)
